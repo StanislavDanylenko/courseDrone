5fec959c4234ce3c2537653b8e684a7ae34b695f
Stanislav Danylenko <s.danylenko@aimprosoft.com>
2018-12-14 21:56:42 +0200

    final state app commit (android)

--- src/main/java/com/stanislav/danylenko/course/web/controller/MobileController.java
+++ src/main/java/com/stanislav/danylenko/course/web/controller/MobileController.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.annotation.JsonView;
 import com.stanislav.danylenko.course.JsonRules;
 import com.stanislav.danylenko.course.db.entity.LocalProposal;
 import com.stanislav.danylenko.course.db.entity.LocalProposalUser;
-import com.stanislav.danylenko.course.db.entity.Proposal;
 import com.stanislav.danylenko.course.db.entity.User;
 import com.stanislav.danylenko.course.db.entity.location.Country;
 import com.stanislav.danylenko.course.db.enumeration.OperationStatus;
 import com.stanislav.danylenko.course.exception.DBException;
-import com.stanislav.danylenko.course.service.*;
+import com.stanislav.danylenko.course.service.LocalProposalService;
+import com.stanislav.danylenko.course.service.LocalProposalUserService;
+import com.stanislav.danylenko.course.service.MobileService;
+import com.stanislav.danylenko.course.service.UserService;
 import com.stanislav.danylenko.course.service.location.CountryService;
 import com.stanislav.danylenko.course.web.model.LocalProposalUserModel;
 import com.stanislav.danylenko.course.web.model.mobile.UserCredentialsModel;
@@ -17,7 +19,6 @@ import com.stanislav.danylenko.course.web.model.user.UpdatePasswordModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -60,7 +61,7 @@ public class MobileController {
 
     @JsonView(value = JsonRules.MobileProposal.class)
     @PostMapping("/proposal/point/{id}")
-    public ResponseEntity<Iterable<LocalProposal>> getCountriesFull(@RequestBody UserCredentialsModel model, @PathVariable Long id) throws DBException {
+    public ResponseEntity<Iterable<LocalProposal>> getProposalsByLocation(@RequestBody UserCredentialsModel model, @PathVariable Long id) throws DBException {
         User user = service.authenticateUser(model.getEmail(), model.getPassword());
         if (user != null) {
             return ResponseEntity.ok(localProposalService.findAllByPopulatedPointAndActivity(id, true));
