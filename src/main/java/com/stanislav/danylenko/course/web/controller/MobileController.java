9b6f83af74f8d59ba532c665a3a4c788e913e857
Stanislav Danylenko <s.danylenko@aimprosoft.com>
2018-12-27 01:54:31 +0200

    fixed mobile controller

--- src/main/java/com/stanislav/danylenko/course/web/controller/MobileController.java
+++ src/main/java/com/stanislav/danylenko/course/web/controller/MobileController.java
@@ -19,11 +19,13 @@ import com.stanislav.danylenko.course.web.model.user.UpdatePasswordModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/mobile")
+@PreAuthorize("hasAuthority('USER')")
 public class MobileController {
 
     @Autowired
@@ -37,72 +39,50 @@ public class MobileController {
     @Autowired
     private LocalProposalUserService localProposalUserService;
 
+
     @JsonView(value = JsonRules.MobileCustom.class)
-    @PostMapping("/{id}")
+    @GetMapping("/{id}")
     public @ResponseBody
-    ResponseEntity<User> getUser(@RequestBody UserCredentialsModel model, @PathVariable Long id) throws DBException {
-        User user = service.authenticateUser(model.getEmail(), model.getPassword());
-        if (user != null) {
-            return new ResponseEntity<>(userService.find(user.getId()), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-        }
+    ResponseEntity<User> getUser(@PathVariable Long id) throws DBException {
+        return new ResponseEntity<>(userService.find(id), HttpStatus.OK);
     }
 
-    @PostMapping("/location/full")
-    public ResponseEntity<Iterable<Country>> getCountriesFull(@RequestBody UserCredentialsModel model) throws DBException {
-        User user = service.authenticateUser(model.getEmail(), model.getPassword());
-        if (user != null) {
-            return ResponseEntity.ok(countryService.findAll());
-        } else {
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-        }
+    @GetMapping("/location/full")
+    public ResponseEntity<Iterable<Country>> getCountriesFull() throws DBException {
+        return ResponseEntity.ok(countryService.findAll());
     }
 
     @JsonView(value = JsonRules.MobileProposal.class)
-    @PostMapping("/proposal/point/{id}")
-    public ResponseEntity<Iterable<LocalProposal>> getProposalsByLocation(@RequestBody UserCredentialsModel model, @PathVariable Long id) throws DBException {
-        User user = service.authenticateUser(model.getEmail(), model.getPassword());
-        if (user != null) {
-            return ResponseEntity.ok(localProposalService.findAllByPopulatedPointAndActivity(id, true));
-        } else {
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-        }
+    @GetMapping("/proposal/point/{id}")
+    public ResponseEntity<Iterable<LocalProposal>> getProposalsByLocation(@PathVariable Long id) throws DBException {
+        return ResponseEntity.ok(localProposalService.findAllByPopulatedPointAndActivity(id, true));
     }
 
     @JsonView(value = JsonRules.MobileProposal.class)
-    @PostMapping("/user/{id}/status/{status}")
+    @GetMapping("/user/{id}/status/{status}")
     public @ResponseBody
-    ResponseEntity<Iterable<LocalProposalUser>> getLocalProposalUserByUserAndStatus(@RequestBody UserCredentialsModel model,
-                                                                                    @PathVariable Long id,
-                                                                                    @PathVariable String status) throws DBException {
+    ResponseEntity<Iterable<LocalProposalUser>> getLocalProposalUserByUserAndStatus(@PathVariable Long id,
+                                                                                    @PathVariable String status)
+                                                                                                throws DBException {
 
-        User user = service.authenticateUser(model.getEmail(), model.getPassword());
-        if (user != null) {
             switch (status) {
                 case "FINALIZED":
-                    return new ResponseEntity<>(localProposalUserService.findAllByUserIdAndStatus(id, OperationStatus.FINALIZED), HttpStatus.OK);
+                    return new ResponseEntity<>(localProposalUserService.findAllByUserIdAndStatus(id, OperationStatus.FINALIZED),
+                            HttpStatus.OK);
                 case "CANCELED":
-                    return new ResponseEntity<>(localProposalUserService.findAllByUserIdAndStatus(id, OperationStatus.CANCELED), HttpStatus.OK);
+                    return new ResponseEntity<>(localProposalUserService.findAllByUserIdAndStatus(id, OperationStatus.CANCELED),
+                            HttpStatus.OK);
                 default:
-                    return new ResponseEntity<>(localProposalUserService.findAllByUserIdAndStatusNot(id, OperationStatus.CANCELED, OperationStatus.FINALIZED), HttpStatus.OK);
+                    return new ResponseEntity<>(localProposalUserService.findAllByUserIdAndStatusNot(id, OperationStatus.CANCELED,
+                            OperationStatus.FINALIZED), HttpStatus.OK);
             }
-        } else {
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-        }
-
     }
 
     @JsonView(value = JsonRules.MobileProposal.class)
-    @PostMapping("/user/{id}")
+    @GetMapping("/user/{id}")
     public @ResponseBody
-    ResponseEntity<Iterable<LocalProposalUser>> getLocalProposalUserByUser(@RequestBody UserCredentialsModel model, @PathVariable Long id) throws DBException {
-        User user = service.authenticateUser(model.getEmail(), model.getPassword());
-        if (user != null) {
-            return new ResponseEntity<>(localProposalUserService.findAllByUserId(id), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-        }
+    ResponseEntity<Iterable<LocalProposalUser>> getLocalProposalUserByUser(@PathVariable Long id) throws DBException {
+        return new ResponseEntity<>(localProposalUserService.findAllByUserId(id), HttpStatus.OK);
     }
 
     @JsonView(value = JsonRules.MobileCustom.class)
